06451937b15316ef858c5aa04d172660fef87db4
David Abarca <63623204+abarcadavid@users.noreply.github.com>
2020-07-19 14:34:33 -0700

    Finished project files.

---
+++ tip/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by David Abarca on 7/18/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        billAmountTextField.keyboardType = UIKeyboardType.decimalPad
+        //billAmountTextField.becomeFirstResponder()
+        
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.10, 0.15, 0.20]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+}
+
